7596c934810e9c40fb874c606cf1e642904931d9
miku1001 <pauloted.22@gmail.com>
2023-05-13 18:20:42 +0800

    TV on and off instance

--- TV.py
+++ TV.py
@@ -7,8 +7,16 @@ class TV:
         self.volume_lvl = volume_lvl
         self.tv_on = tv_on
 
-# instance method (turn on TV)
-# instance method (turn off TV)
+    # instance method (turn on TV)
+    def turn_on(self):
+        self.tv_on = True
+        print(f"The TV is on")
+
+            
+    # instance method (turn off TV)
+    def turn_off(self):
+        self.tv_on = False
+
 # instance method (return channel)
 # instance method (set new channel)
 # instance method (return volume)
