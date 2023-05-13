66b247634d494b701e28dd6f71b5a05b9780bddb
miku1001 <pauloted.22@gmail.com>
2023-05-13 18:15:24 +0800

    constructor and instance variable

--- TV.py
+++ TV.py
@@ -1,6 +1,12 @@
 # create class named TV
-# create constructor
-# create instance variables
+class TV:
+    # create constructor
+    def __init__(self, channel=1, volume_lvl=0, tv_on=False):
+        # create instance variables
+        self.channel = channel
+        self.volume_lvl = volume_lvl
+        self.tv_on = tv_on
+
 # instance method (turn on TV)
 # instance method (turn off TV)
 # instance method (return channel)
