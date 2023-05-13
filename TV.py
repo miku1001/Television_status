4c6081997a11696945f6b91e658e98e7b90090c0
miku1001 <pauloted.22@gmail.com>
2023-05-13 17:59:22 +0800

    Pseudocodes

---
+++ TV.py
@@ -0,0 +1,13 @@
+# create class named TV
+# create constructor
+# create instance variables
+# instance method (turn on TV)
+# instance method (turn off TV)
+# instance method (return channel)
+# instance method (set new channel)
+# instance method (return volume)
+# instance method (set new channel)
+# instance method (increase channel by 1)
+# instance method (decrease channel by 1)
+# instance method (increase volume by 1)
+# instance method (decrease volume by 1)
